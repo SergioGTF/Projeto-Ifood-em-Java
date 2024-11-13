86bedbf48fd5339d9dfc77bfd4b6801c24e5177c
SergioGTF <sergio23300033@aluno.cesupa.br>
2024-11-13 20:58:02 -0300

    Projeto Ifood

---
+++ src/main/java/com/mycompany/telaprincipal/TelaPrincipal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.telaprincipal;
+
+/**
+ *
+ * @author sergi
+ */
+public class TelaPrincipal {
+
+    public static void main(String[] args) {
+        new TelaLogin().setVisible(true);
+    }
+}
